f70e2b25043674cfe9b18e03576ef0ce98717c97
shahjalalk <shahjalalkhan215@gmail.com>
2026-04-28 13:02:06 +0600

    ks

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -17,13 +17,13 @@ export async function POST(request: Request) {
 
     const secretKey = process.env.TURNSTILE_SECRET_KEY;
     const verifyResponse = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        secret: secretKey,
-        response: captchaToken,
-      }),
-    });
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, // হেডার পরিবর্তন
+        body: new URLSearchParams({
+          secret: secretKey || "",
+          response: captchaToken,
+        }),
+      });
 
     const verificationData = await verifyResponse.json();
 
